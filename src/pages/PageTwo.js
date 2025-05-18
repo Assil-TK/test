d763b5c53f1abd386f936f79d219349f564512b4
Assil Tekaya <154938421+Assil-TK@users.noreply.github.com>
2025-05-18 16:57:59 +0100

    hello

--- src/pages/PageTwo.js
+++ src/pages/PageTwo.js
@@ -2,9 +2,10 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 import { styled } from "@mui/system";
+import { Typography } from "@mui/material";
 
 const RedButton = styled(Button)({
-  backgroundColor: 'blue',
+  backgroundColor: 'red',
   '&:hover': {
     backgroundColor: '#F39325',
   },
@@ -14,14 +15,16 @@ function PageTwo() {
   const navigate = useNavigate();
 
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
+    <div style={{ padding: "20px", textAlign: "center", backgroundColor: '#F5F5F6', fontFamily: 'Fira Sans' }}>
       <h1>Page Two</h1>
       <RedButton
         variant="contained"
         onClick={() => navigate("/")}
+        sx={{ fontFamily: 'Fira Sans' }}
       >
         Go to Page 1
       </RedButton>
+      <Typography sx={{ fontFamily: 'Fira Sans', color: '#1B374C' }}>Hello</Typography>
     </div>
   );
 }
